eb0f6feac9da6b6824b09af2357fc32cad54cb5f
Oliver-Zane <chetanhs123@gmail.com>
2025-10-06 12:20:50 +0530

    Added and Tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -3,7 +3,7 @@ Unit Tests for Calculator
 Students start with 2 passing tests, then add more
 """
 import pytest
-from src.calculator import add, divide, subtract
+from src.calculator import add, divide, subtract, multiply
 
 class TestBasicOperations:
     """Test basic arithmetic operations"""
@@ -33,4 +33,27 @@ class TestMultiplyDivideWithValidation:
         with pytest.raises(TypeError, match="Division requires numeric inputs"):
             divide("10", 2)
 
+#TestBasicOperations
+class TestBasicOperations:
+    """Test basic arithmetic operations"""
+    def test_add_positive_numbers(self):
+        """Test adding positive numbers"""
+        assert add(2, 3) == 5
+        assert add(10, 15) == 25
+
+    def test_subtract_positive_numbers(self):
+        """Test subtracting positive numbers"""
+        assert subtract(5, 3) == 2
+        assert subtract(10, 4) == 6
+
+    # MAKE SURE THESE NEXT TWO FUNCTIONS ARE INDENTED JUST LIKE THE ONES ABOVE
+    def test_add_negative_numbers(self):
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
+
+    def test_subtract_negative_numbers(self):
+        """Test subtracting negative numbers"""
+        assert subtract(-1, -1) == 0
+        assert subtract(-5, -3) == -2
 # TODO: Students will add TestMultiplyDivide class
